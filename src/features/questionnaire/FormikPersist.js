62b8b32895d46d7f820349021a66246fe8ad5d18
Jhemys Alves <jhemys@outlook.com>
2021-05-16 20:36:05 -0300

    Persisting form data while refreshing page

---
+++ src/features/questionnaire/FormikPersist.js
@@ -0,0 +1,55 @@
+import React, { PureComponent } from 'react'
+import { FormikConsumer } from 'formik'
+
+class FormikPersistor extends PureComponent {
+  componentWillMount() {
+    window.addEventListener('beforeunload', this.clear)
+  }
+
+  componentDidMount() {
+    const { setValues, setErrors } = this.props
+    const data = window.localStorage.getItem(this.storageKey)
+    if (data) {
+      const { values, errors } = JSON.parse(data)
+      setValues(values)
+      setErrors(errors)
+    }
+  }
+
+  componentDidUpdate() {
+    const { values, errors } = this.props
+    window.localStorage.setItem(this.storageKey, JSON.stringify({ values, errors }))
+  }
+
+  // componentWillUnmount() {
+  //   window.removeEventListener('beforeunload', this.clear)
+  // }
+
+  get storageKey() {
+    return `formik.form.${this.props.name}`
+  }
+
+  // clear = () => {
+  //   window.localStorage.removeItem(this.storageKey)
+  // }
+
+  render() {
+    return null
+  }
+}
+
+const FormikPersist = ({ name }) => (
+  <FormikConsumer>
+    {({ values, errors, setValues, setErrors }) => (
+      <FormikPersistor
+        name={name}
+        setValues={setValues}
+        setErrors={setErrors}
+        values={values}
+        errors={errors}
+      />
+    )}
+  </FormikConsumer>
+)
+
+export default FormikPersist
--- src/features/questionnaire/SurveyForm.js
+++ src/features/questionnaire/SurveyForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import Chip from '@material-ui/core/Chip';
+import Chip from "@material-ui/core/Chip";
 import Questionnaire from "./Questionnaire";
 import { Formik, Form, Field } from "formik";
+import FormikPersist from './FormikPersist';
 
 const SurveyForm = () => {
   const [position, setSurveyPosition] = useState(0);
@@ -13,48 +14,77 @@ const SurveyForm = () => {
   return (
     <Formik
       initialValues={{ name: "", age: "", gender: "", questions: [] }}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log(values);
+        setSubmitting(false);
+      }}
     >
       {(props) => (
         <Form onSubmit={props.handleSubmit}>
-          { position === 0 && <p>
-            <img src="assets/css/images/logo.png" alt="a" border={0} />
-          </p> }
+          {position === 0 && (
+            <p>
+              <img src="assets/css/images/logo.png" alt="a" border={0} />
+            </p>
+          )}
           <div className={position === 0 ? "current-survey" : "hidden-survey"}>
             <p>Olá, como você gosta de ser chamado/a?</p>
             <p>
-              <Field type="text" className="inputForm" name="name" placeholder="Nome" onBlur={() => toggleWelcomeMessage(true)} />
+              <Field
+                type="text"
+                className="inputForm"
+                name="name"
+                placeholder="Nome"
+                onBlur={() => toggleWelcomeMessage(true)}
+              />
             </p>
-            {showWelcomeMessage && <p className="textName questao-respondida">Que bom te ver aqui, {props.values.name}!</p> }
+            {showWelcomeMessage && (
+              <p className="textName questao-respondida">
+                Que bom te ver aqui, {props.values.name}!
+              </p>
+            )}
             <p>Quando é seu aniversário?</p>
             <p>
-              <Field type="number" className="inputForm" name="age" placeholder="Idade" />
+              <Field
+                type="number"
+                className="inputForm"
+                name="age"
+                placeholder="Idade"
+              />
             </p>
             <p>Você gostaria de informar seu gênero?</p>
             <p>
               <Chip
                 label="Mulher"
-                
-                onClick={() => { props.setFieldValue('gender', 'F'); } }
-                variant={props.values.gender === 'F' ? undefined : "outlined"}
+                onClick={() => {
+                  props.setFieldValue("gender", "F");
+                }}
+                variant={props.values.gender === "F" ? undefined : "outlined"}
                 className="btnPadrão"
               />
               <Chip
                 label="Homem"
-                onClick={() => { props.setFieldValue('gender', 'M'); } }
-                variant={props.values.gender === 'M' ? undefined : "outlined"}
+                onClick={() => {
+                  props.setFieldValue("gender", "M");
+                }}
+                variant={props.values.gender === "M" ? undefined : "outlined"}
                 className="btnPadrão"
               />
               <Chip
                 label="Não gostaria"
-                onClick={() => { props.setFieldValue('gender', 'X'); } }
-                variant={props.values.gender === 'X' ? undefined : "outlined"}
+                onClick={() => {
+                  props.setFieldValue("gender", "X");
+                }}
+                variant={props.values.gender === "X" ? undefined : "outlined"}
                 className="btnPadrão"
               />
             </p>
-            <p><button type="button" onClick={() => setSurveyPosition(position + 1)}>
-              Next
-            </button>
+            <p>
+              <button
+                type="button"
+                onClick={() => setSurveyPosition(position + 1)}
+              >
+                Next
+              </button>
             </p>
           </div>
           <Questionnaire
@@ -62,37 +92,45 @@ const SurveyForm = () => {
             prevQuestion={prevQuestion}
             position={position}
           />
-          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+          {/* <AutoSave debounceMs={1000} /> */}
           
-      { position === 0 && <nav>
-        <ul>
-          <li>
-            <a href="www.google.com" className="icon brands fa-twitter">
-              <span className="label">Twitter</span>
-            </a>
-          </li>
-          <li>
-            <a href="www.google.com" className="icon brands fa-facebook-f">
-              <span className="label">Facebook</span>
-            </a>
-          </li>
-          <li>
-            <a href="www.google.com" className="icon brands fa-dribbble">
-              <span className="label">Dribbble</span>
-            </a>
-          </li>
-          <li>
-            <a href="www.google.com" className="icon brands fa-github">
-              <span className="label">Github</span>
-            </a>
-          </li>
-          <li>
-            <a href="www.google.com" className="icon solid fa-envelope">
-              <span className="label">Email</span>
-            </a>
-          </li>
-        </ul>
-      </nav>}
+          <FormikPersist name="form" />
+          {props.errors.name && <div id="feedback">{props.errors.name}</div>}
+
+          {position === 0 && (
+            <nav>
+              <ul>
+                <li>
+                  <a href="www.google.com" className="icon brands fa-twitter">
+                    <span className="label">Twitter</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="www.google.com"
+                    className="icon brands fa-facebook-f"
+                  >
+                    <span className="label">Facebook</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="www.google.com" className="icon brands fa-dribbble">
+                    <span className="label">Dribbble</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="www.google.com" className="icon brands fa-github">
+                    <span className="label">Github</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="www.google.com" className="icon solid fa-envelope">
+                    <span className="label">Email</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          )}
         </Form>
       )}
     </Formik>
